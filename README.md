0b99e4f9efcbd88eca51fc1ee535a3201b6cadc0
sway2020 <65274619+sway2020@users.noreply.github.com>
2020-05-14 13:37:41 -0700

    Update README & create archive folder

--- README.md
+++ README.md
@@ -1,6 +1,58 @@
 # GravitativeCableCar
 Gravitative Cable Car mod for Cities Skylines.
 
-This mod was written in the prefix method of Harmony so it can be incompatible with other mods which also change how cable cars are rendered. Right now this is just a proof-of-concept thing so I don't plan to spend too much time on it. I'll rewrite it using the transpiler method when I have more time and a known incompatibility with another mod occurs.
+## Warning
+This mod was written in the prefix method of Harmony so it will be incompatible with other mods which also change how cable cars are rendered. Right now this is just a proof-of-concept thing and cable cars are not popular so....I'll rewrite it using the transpiler method when I have more time to learn about it and a known incompatibility with another mod occurs.
 
+## Release Note
+v0.1 - Proof of concept. Change the rotation of meshes for rendering.
+
+## Required Items
+Harmony: this mod overwrites a function the game uses to render cable cars. boformer's [Harmony mod](https://steamcommunity.com/workshop/filedetails/?id=2040656402) makes this a lot easier.
+Mass Transit DLC: You need this DLC to use cable cars.
+
+## Goal & Future Plan
+The goal of this mod is to make cable cars move more realistically(from the viewpo
+
+## To-do
+- Rewrite using the transpiler method.
+
+## Contact
+Steam workhsop [link](https://steamcommunity.com/sharedfiles/filedetails/?id=2094830335)  
 This is not my main Github account so you probably would get a faster response if you send me a Steam message
+
+
+## FAQ
+
+### Does it work with existing cable cars on the workshop?
+Unfortunately at the moment only my own [colored cable car](https://steamcommunity.com/sharedfiles/filedetails/?id=2088780350) asset, and this [modified version of vanilla cable car](https://steamcommunity.com/sharedfiles/filedetails/?id=2095140359) would rotate fully in the intended way because I already modified them for testing.
+
+Vanilla cable cars and other cable car assets, without modification, would have the whole vehicle rotated like the third screenshot shows(see the wheels). I think this still looks better than the original. Now we have the smaller top/wheel part, not the cabin part, against the law of physics, and this is nearly unnoticeable when you watch from a long distance. I wrote a short how-to guide at the end of FAQ for other asset creators who are interested in making their cable car assets compatible with this mod.
+
+### Incompatibility with other mod?
+Unknown. This mod is not widely tested yet. This mod was written in the prefix method of Harmony so it will be incompatible with other mods which also change how cable cars are rendered. Right now this is just a proof-of-concept thing and cable cars are not popular so....I'll rewrite it using the transpiler method when I have more time to learn about it and a known incompatibility with another mod occurs.
+
+### Performance
+This is a lightweight simple mod which renders cable car instances with a modified version of the RenderInstance method in the Vehicle class. The performance difference should be minimal and unnoticeable.
+
+### Known Issues and Bugs
+This mod is still in the early development stage and it's more of a proof of concept now. If you encounter any issue/bug or know how to fix it please let me know.
+
+Issue 1. It seems like vehicle shaders don't support submesh LOD, and the submesh would disappear upon reaching LOD distance. In our case it means the top/wheel part of the cable car would disappear when it switches to LOD. I can think of 3 ways to tackle this issue:
+
+A. Something similar to what the [Transparency LODs Fix](https://steamcommunity.com/workshop/filedetails/?id=922939393) mod does. This option gives the best result but it would increase performance burden.
+
+B. Combine all submesh LODs with the main LOD. This means the top/wheel LOD would rotate together with other parts(not staying on the cables) so it looks a bit weird.
+
+C. Just let the top/wheel part disappear when the game switches to LOD.
+
+Option A needs more modding work. Asset creators can choose between B or C on their own just by re-organizing their assets. Right now my colored cable car asset uses option B because unlike transparent buildings, cable cars are small enough so it isn't obvious from a far distance. I'm still open to other options.
+
+### Can this mod break my save file?
+This mod does not touch you save data, game physics, or any other game data at all so it is unlikely this mod would break you save file. This mod only changes how cable cars are rendered in runtime and you can disable this mod any time. However it is always a good idea to back up your save file periodically. If it turns out this mod is the reason why your save file is broken, I'm not responsible for it although I will do my best to help you out.
+
+### What can I do to help the development of this mod?
+Any comment/feedback is welcome! I decided to publish this PoC 0.1 version so I can collect bug/issue reports and suggestions before I start rewriting this mod in a more proper way. Although I have prior experience in other programming languages, I am not familiar with C#, Harmony or Unity at all. If you read the source code and find any issue or know a better way to make things work, feel free to comment below or use the issues tab on Github.
+
+### How do I make my own cable car assets compatible with this mod?
+Right now the mod changes the quaternion rotation of the main mesh and sub-meshes for rendering. The only exception is sub-mesh 1 which still uses the original rotation. So just put the wheels/top part of your cable car in submesh 1. Any other meshes will have zero rotation on the x and z axes so it looks like they're subject to gravity. I plan to make this more customizable in the future and I'd love to get suggestion from other cable car asset creators. If you have any question feel free to send me a message.
---
+++ archive/v0.1/GravitativeCableCar.cs
@@ -0,0 +1,358 @@
+﻿using CitiesHarmony.API;
+using HarmonyLib;
+using ICities;
+using System;
+using System.Reflection;
+using UnityEngine;
+using ColossalFramework;
+
+namespace GravitativeCableCar
+{
+    public class Mod : IUserMod
+    {
+
+        public string Name => "Gravitative Cable Car";
+        public string Description => "make cable cars subject to gravity";
+
+        public void OnEnabled()
+        {
+            HarmonyHelper.DoOnHarmonyReady(() => Patcher.PatchAll());
+        }
+
+        public void OnDisabled()
+        {
+            if (HarmonyHelper.IsHarmonyInstalled) Patcher.UnpatchAll();
+        }
+    }
+
+    public static class Patcher
+    {
+        private const string HarmonyId = "sway.GravitativeCableCar";
+
+        private static bool patched = false;
+
+        public static void PatchAll()
+        {
+            if (patched) return;
+
+            UnityEngine.Debug.Log("Gravitative Cable Car: Patching...");
+
+            patched = true;
+
+            // Harmony.DEBUG = true;
+            var harmony = new Harmony("sway.GravitativeCableCar");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static void UnpatchAll()
+        {
+            if (!patched) return;
+
+            var harmony = new Harmony(HarmonyId);
+            harmony.UnpatchAll(HarmonyId);
+
+            patched = false;
+
+            UnityEngine.Debug.Log("Gravitative Cable Car: Reverted...");
+        }
+    }
+
+    // Modified from Vehicle's RenderInstance method
+    [HarmonyPatch(typeof(Vehicle), "RenderInstance", new Type[] { typeof(RenderManager.CameraInfo), typeof(VehicleInfo), typeof(Vector3), typeof(Quaternion), typeof(Vector3), typeof(Vector4), typeof(Vector4), typeof(Vector3), typeof(float), typeof(Color), typeof(Vehicle.Flags), typeof(int), typeof(InstanceID), typeof(bool), typeof(bool) })]
+    public static class VehicleRenderInstancePatch
+    {
+        // Run before the original method. Skip the original one if the vehicle is a cable car
+        public static bool Prefix(RenderManager.CameraInfo cameraInfo, VehicleInfo info, Vector3 position, ref Quaternion rotation, Vector3 swayPosition, Vector4 lightState, Vector4 tyrePosition, Vector3 velocity, float acceleration, Color color, Vehicle.Flags flags, int variationMask, InstanceID id, bool underground, bool overground)
+        {
+            // if the vehicle is not a cable car, skip and use the original method
+            if ((int)info.m_vehicleType != 0x1000){
+                return true;
+            }
+            
+            // Limit rotation along the x and z axes
+            Quaternion originalRotation = rotation;
+            rotation.x = 0;
+            rotation.z = 0;
+
+            if ((cameraInfo.m_layerMask & (1 << info.m_prefabDataLayer)) == 0)
+            {
+                // return false to skip the original method
+                return false;
+            }
+            Vector3 scale = Vector3.one;
+            if ((flags & Vehicle.Flags.Inverted) != 0)
+            {
+                scale = new Vector3(-1f, 1f, -1f);
+                Vector4 vector = lightState;
+                lightState.x = vector.y;
+                lightState.y = vector.x;
+                lightState.z = vector.w;
+                lightState.w = vector.z;
+            }
+
+            info.m_vehicleAI.RenderExtraStuff(id.Vehicle, ref Singleton<VehicleManager>.instance.m_vehicles.m_buffer[id.Vehicle], cameraInfo, id, position, rotation, tyrePosition, lightState, scale, swayPosition, underground, overground);
+            
+            if (cameraInfo.CheckRenderDistance(position, info.m_lodRenderDistance))
+            {
+                VehicleManager instance = Singleton<VehicleManager>.instance;
+                Matrix4x4 bodyMatrix = info.m_vehicleAI.CalculateBodyMatrix(flags, ref position, ref rotation, ref scale, ref swayPosition);
+                Matrix4x4 originalBodyMatrix = info.m_vehicleAI.CalculateBodyMatrix(flags, ref position, ref originalRotation, ref scale, ref swayPosition);
+                Matrix4x4 value = info.m_vehicleAI.CalculateTyreMatrix(flags, ref position, ref rotation, ref scale, ref bodyMatrix);
+                if (Singleton<InfoManager>.instance.CurrentMode == InfoManager.InfoMode.None)
+                {
+                    RenderGroup.MeshData effectMeshData = info.m_vehicleAI.GetEffectMeshData();
+                    EffectInfo.SpawnArea area = new EffectInfo.SpawnArea(bodyMatrix, effectMeshData, info.m_generatedInfo.m_tyres, info.m_lightPositions);
+                    if (info.m_effects != null)
+                    {
+                        for (int i = 0; i < info.m_effects.Length; i++)
+                        {
+                            VehicleInfo.Effect effect = info.m_effects[i];
+                            if (((effect.m_vehicleFlagsRequired | effect.m_vehicleFlagsForbidden) & flags) == effect.m_vehicleFlagsRequired && effect.m_parkedFlagsRequired == VehicleParked.Flags.None)
+                            {
+                                effect.m_effect.RenderEffect(id, area, velocity, acceleration, 1f, -1f, Singleton<SimulationManager>.instance.m_simulationTimeDelta, cameraInfo);
+                            }
+                        }
+                    }
+                }
+                if ((flags & Vehicle.Flags.Inverted) != 0)
+                {
+                    tyrePosition.x = 0f - tyrePosition.x;
+                    tyrePosition.y = 0f - tyrePosition.y;
+                }
+                MaterialPropertyBlock materialBlock = instance.m_materialBlock;
+                materialBlock.Clear();
+                materialBlock.SetMatrix(instance.ID_TyreMatrix, value);
+                materialBlock.SetVector(instance.ID_TyrePosition, tyrePosition);
+                materialBlock.SetVector(instance.ID_LightState, lightState);
+                bool flag = Singleton<ToolManager>.instance.m_properties.m_mode == ItemClass.Availability.AssetEditor;
+                if (!flag)
+                {
+                    materialBlock.SetColor(instance.ID_Color, color);
+                }
+                bool flag2 = true;
+                if (flag)
+                {
+                    flag2 = BuildingDecoration.IsMainMeshRendered();
+                }
+                if (info.m_subMeshes != null)
+                {
+                    for (int j = 0; j < info.m_subMeshes.Length; j++)
+                    {
+                        VehicleInfo.MeshInfo meshInfo = info.m_subMeshes[j];
+                        VehicleInfoBase subInfo = meshInfo.m_subInfo;
+                        if ((!flag && ((meshInfo.m_vehicleFlagsRequired | meshInfo.m_vehicleFlagsForbidden) & flags) == meshInfo.m_vehicleFlagsRequired && (meshInfo.m_variationMask & variationMask) == 0 && meshInfo.m_parkedFlagsRequired == VehicleParked.Flags.None) || (flag && BuildingDecoration.IsSubMeshRendered(subInfo)))
+                        {
+                            if (!(subInfo != null))
+                            {
+                                continue;
+                            }
+                            instance.m_drawCallData.m_defaultCalls++;
+                            if (underground)
+                            {
+                                if (subInfo.m_undergroundMaterial == null && subInfo.m_material != null)
+                                {
+                                    VehicleProperties properties = instance.m_properties;
+                                    if (properties != null)
+                                    {
+                                        subInfo.m_undergroundMaterial = new Material(properties.m_undergroundShader);
+                                        subInfo.m_undergroundMaterial.CopyPropertiesFromMaterial(subInfo.m_material);
+                                    }
+                                }
+                                subInfo.m_undergroundMaterial.SetVectorArray(instance.ID_TyreLocation, subInfo.m_generatedInfo.m_tyres);
+                                if (j == 1)
+                                {
+                                    Graphics.DrawMesh(subInfo.m_mesh, originalBodyMatrix, subInfo.m_undergroundMaterial, instance.m_undergroundLayer, null, 0, materialBlock);
+                                }
+                                else
+                                {
+                                    Graphics.DrawMesh(subInfo.m_mesh, bodyMatrix, subInfo.m_undergroundMaterial, instance.m_undergroundLayer, null, 0, materialBlock);
+                                }
+                            }
+                            if (overground)
+                            {
+                                subInfo.m_material.SetVectorArray(instance.ID_TyreLocation, subInfo.m_generatedInfo.m_tyres);
+                                if (j == 1)
+                                {
+                                    Graphics.DrawMesh(subInfo.m_mesh, originalBodyMatrix, subInfo.m_material, info.m_prefabDataLayer, null, 0, materialBlock);
+                                }
+                                else
+                                {
+                                    Graphics.DrawMesh(subInfo.m_mesh, bodyMatrix, subInfo.m_material, info.m_prefabDataLayer, null, 0, materialBlock);
+                                }
+                            }
+                        }
+                        else if (subInfo == null)
+                        {
+                            flag2 = false;
+                        }
+                    }
+                }
+                if (!flag2)
+                {
+                    // return false to skip the original method
+                    return false;
+                }
+                instance.m_drawCallData.m_defaultCalls++;
+                if (underground)
+                {
+                    if (info.m_undergroundMaterial == null && info.m_material != null)
+                    {
+                        VehicleProperties properties2 = instance.m_properties;
+                        if (properties2 != null)
+                        {
+                            info.m_undergroundMaterial = new Material(properties2.m_undergroundShader);
+                            info.m_undergroundMaterial.CopyPropertiesFromMaterial(info.m_material);
+                        }
+                    }
+                    info.m_undergroundMaterial.SetVectorArray(instance.ID_TyreLocation, info.m_generatedInfo.m_tyres);
+                    Graphics.DrawMesh(info.m_mesh, bodyMatrix, info.m_undergroundMaterial, instance.m_undergroundLayer, null, 0, materialBlock);
+                }
+                if (overground)
+                {
+                    info.m_material.SetVectorArray(instance.ID_TyreLocation, info.m_generatedInfo.m_tyres);
+                    Graphics.DrawMesh(info.m_mesh, bodyMatrix, info.m_material, info.m_prefabDataLayer, null, 0, materialBlock);
+                }
+                // return false to skip the original method
+                return false;
+            }
+            Matrix4x4 bodyMatrix2 = info.m_vehicleAI.CalculateBodyMatrix(flags, ref position, ref rotation, ref scale, ref swayPosition);
+            Matrix4x4 originalBodyMatrix2 = info.m_vehicleAI.CalculateBodyMatrix(flags, ref position, ref originalRotation, ref scale, ref swayPosition);
+            if (Singleton<ToolManager>.instance.m_properties.m_mode == ItemClass.Availability.AssetEditor)
+            {
+                Matrix4x4 value2 = info.m_vehicleAI.CalculateTyreMatrix(flags, ref position, ref rotation, ref scale, ref bodyMatrix2);
+                VehicleManager instance2 = Singleton<VehicleManager>.instance;
+                MaterialPropertyBlock materialBlock2 = instance2.m_materialBlock;
+                materialBlock2.Clear();
+                materialBlock2.SetMatrix(instance2.ID_TyreMatrix, value2);
+                materialBlock2.SetVector(instance2.ID_TyrePosition, tyrePosition);
+                materialBlock2.SetVector(instance2.ID_LightState, lightState);
+                Mesh mesh = null;
+                Material material = null;
+                if (info.m_lodObject != null)
+                {
+                    MeshFilter component = info.m_lodObject.GetComponent<MeshFilter>();
+                    if (component != null)
+                    {
+                        mesh = component.sharedMesh;
+                    }
+                    Renderer component2 = info.m_lodObject.GetComponent<Renderer>();
+                    if (component2 != null)
+                    {
+                        material = component2.sharedMaterial;
+                    }
+                }
+                if (mesh != null && material != null)
+                {
+                    materialBlock2.SetVectorArray(instance2.ID_TyreLocation, info.m_generatedInfo.m_tyres);
+                    Graphics.DrawMesh(mesh, bodyMatrix2, material, info.m_prefabDataLayer, null, 0, materialBlock2);
+                }
+            }
+            else if (Singleton<InfoManager>.instance.CurrentMode == InfoManager.InfoMode.None)
+            {
+                RenderGroup.MeshData effectMeshData2 = info.m_vehicleAI.GetEffectMeshData();
+                EffectInfo.SpawnArea area2 = new EffectInfo.SpawnArea(bodyMatrix2, effectMeshData2, info.m_generatedInfo.m_tyres, info.m_lightPositions);
+                if (info.m_effects != null)
+                {
+                    for (int k = 0; k < info.m_effects.Length; k++)
+                    {
+                        VehicleInfo.Effect effect2 = info.m_effects[k];
+                        if (((effect2.m_vehicleFlagsRequired | effect2.m_vehicleFlagsForbidden) & flags) == effect2.m_vehicleFlagsRequired && effect2.m_parkedFlagsRequired == VehicleParked.Flags.None)
+                        {
+                            effect2.m_effect.RenderEffect(id, area2, velocity, acceleration, 1f, -1f, Singleton<SimulationManager>.instance.m_simulationTimeDelta, cameraInfo);
+                        }
+                    }
+                }
+            }
+            bool flag3 = true;
+            if (info.m_subMeshes != null)
+            {
+                for (int l = 0; l < info.m_subMeshes.Length; l++)
+                {
+                    VehicleInfo.MeshInfo meshInfo2 = info.m_subMeshes[l];
+                    VehicleInfoBase subInfo2 = meshInfo2.m_subInfo;
+                    if (((meshInfo2.m_vehicleFlagsRequired | meshInfo2.m_vehicleFlagsForbidden) & flags) == meshInfo2.m_vehicleFlagsRequired && (meshInfo2.m_variationMask & variationMask) == 0 && meshInfo2.m_parkedFlagsRequired == VehicleParked.Flags.None)
+                    {
+                        if (!(subInfo2 != null))
+                        {
+                            continue;
+                        }
+                        if (underground)
+                        {
+
+                            if (l == 1)
+                            {
+                                subInfo2.m_undergroundLodTransforms[subInfo2.m_undergroundLodCount] = originalBodyMatrix2;
+                            }
+                            else
+                            {
+                                subInfo2.m_undergroundLodTransforms[subInfo2.m_undergroundLodCount] = bodyMatrix2;
+                            }
+                        
+                            subInfo2.m_undergroundLodLightStates[subInfo2.m_undergroundLodCount] = lightState;
+                            subInfo2.m_undergroundLodColors[subInfo2.m_undergroundLodCount] = color.linear;
+                            subInfo2.m_undergroundLodMin = Vector3.Min(subInfo2.m_undergroundLodMin, position);
+                            subInfo2.m_undergroundLodMax = Vector3.Max(subInfo2.m_undergroundLodMax, position);
+                            if (++subInfo2.m_undergroundLodCount == subInfo2.m_undergroundLodTransforms.Length)
+                            {
+                                Vehicle.RenderUndergroundLod(cameraInfo, subInfo2);
+                            }
+                        }
+                        if (overground)
+                        {
+                            if (l == 1)
+                            {
+                                subInfo2.m_lodTransforms[subInfo2.m_lodCount] = originalBodyMatrix2;
+                            }
+                            else
+                            {
+                                subInfo2.m_lodTransforms[subInfo2.m_lodCount] = bodyMatrix2;
+                            }
+                            subInfo2.m_lodLightStates[subInfo2.m_lodCount] = lightState;
+                            subInfo2.m_lodColors[subInfo2.m_lodCount] = color.linear;
+                            subInfo2.m_lodMin = Vector3.Min(subInfo2.m_lodMin, position);
+                            subInfo2.m_lodMax = Vector3.Max(subInfo2.m_lodMax, position);
+                            if (++subInfo2.m_lodCount == subInfo2.m_lodTransforms.Length)
+                            {
+                                Vehicle.RenderLod(cameraInfo, subInfo2);
+                            }
+                        }
+                    }
+                    else if (subInfo2 == null)
+                    {
+                        flag3 = false;
+                    }
+                }
+            }
+            if (!flag3)
+            {
+                return false;
+            }
+            if (underground)
+            {
+                info.m_undergroundLodTransforms[info.m_undergroundLodCount] = bodyMatrix2;
+                info.m_undergroundLodLightStates[info.m_undergroundLodCount] = lightState;
+                info.m_undergroundLodColors[info.m_undergroundLodCount] = color.linear;
+                info.m_undergroundLodMin = Vector3.Min(info.m_undergroundLodMin, position);
+                info.m_undergroundLodMax = Vector3.Max(info.m_undergroundLodMax, position);
+                if (++info.m_undergroundLodCount == info.m_undergroundLodTransforms.Length)
+                {
+                    Vehicle.RenderUndergroundLod(cameraInfo, info);
+                }
+            }
+            if (overground)
+            {
+                info.m_lodTransforms[info.m_lodCount] = bodyMatrix2;
+                info.m_lodLightStates[info.m_lodCount] = lightState;
+                info.m_lodColors[info.m_lodCount] = color.linear;
+                info.m_lodMin = Vector3.Min(info.m_lodMin, position);
+                info.m_lodMax = Vector3.Max(info.m_lodMax, position);
+                if (++info.m_lodCount == info.m_lodTransforms.Length)
+                {
+                    Vehicle.RenderLod(cameraInfo, info);
+                }
+            }
+            // return false to skip the original method
+            return false;
+        }
+    }
+}
